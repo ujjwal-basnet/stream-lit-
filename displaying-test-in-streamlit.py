dcf892c32bff3d69355ab20d546021643142684a
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-10 17:01:15 +0545

    git initilized

---
+++ displaying-test-in-streamlit.py
@@ -0,0 +1,23 @@
+#core pkgs
+import streamlit as st 
+
+# Additional pkgs 
+
+# Fxns 
+
+st.text("Hellow world this is  text")
+name  ='ujjwal'
+st.text("This is so {}".format(name))
+
+#header 
+st.header("This is a header ")
+
+#subheader 
+st.subheader("This is a subheader")
+
+#title 
+st.title("This is a title")
+
+#Markdown 
+st.markdown("This is markdown")
+
