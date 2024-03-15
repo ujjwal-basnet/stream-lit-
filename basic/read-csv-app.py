062db5e9321353178f5d55ffe88f3ab9a0eb940b
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-15 12:56:46 +0545

    create a basic folder , create read-csv-app.py and display-text

---
+++ basic/read-csv-app.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import pandas as pd 
+from sklearn import datasets 
+
+
+
+def main() :
+#load datasets
+    iris_ = datasets.load_iris()
+    df = pd.DataFrame(iris_.data,columns=iris_.feature_names)
+    st.dataframe(df) #or st.write(df)
+    
+    st.header("Printing only head (5 values)")
+    st.dataframe(df.head()) # or st.write(df.head())
+    
+    st.header("print from last" )
+    st.dataframe(df.tail()) # or st.write(df.tail())
+if __name__ == '__main__':
+    main()
+    
+    
+
