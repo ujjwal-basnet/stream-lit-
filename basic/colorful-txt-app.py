c9a4e60d63580fd771faca77b4d856b5c67201b7
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-15 13:18:07 +0545

    about sucess , warning ,ingo,error,exception

---
+++ basic/colorful-txt-app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+def main():
+    st.success("This is Sucessfull")
+    st.warning ("This is Warning")
+    st.info("This is information")
+    st.error("This is error")
+    st.exception("This is exception")
+        
+
+
+if __name__ == '__main__':
+ main()
